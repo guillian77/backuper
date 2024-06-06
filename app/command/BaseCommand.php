e7d34d3441d3b5e8b9a156574a68ff8c265f3beb
Guillian Aufrère <guillian77270@gmail.com>
2024-06-06 13:13:33 +0200

    feat: implement db:truncate:table command

--- app/command/BaseCommand.php
+++ app/command/BaseCommand.php
@@ -2,19 +2,36 @@
 
 namespace App\Command;
 
+use App\App;
 use App\Service\OutputService;
+use SQLite3;
 
 abstract class BaseCommand
 {
     protected OutputService $output;
+    protected SQLite3 $conn;
 
     public string $commandName;
-
     public string $commandDescription;
+    private array $argv;
 
-    public function __construct()
+    public function __construct(array $argv)
     {
         $this->output = new OutputService();
+        
+        $this->argv = $argv;
+
+        $this->conn = App::get()->getDb()->conn;
+    }
+
+    protected function getParameter(int $number, mixed $default = null)
+    {
+        if (!isset($this->argv[$number])) {
+            return $default;
+        }
+
+
+        return $this->argv[$number];
     }
 
     abstract public function execute(): void;
---
+++ app/command/DbTableTruncate.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Command;
+
+use App\App;
+
+class DbTableTruncate extends BaseCommand
+{
+    const DB_TABLES = [
+        'backup_history',
+        'configuration',
+        'directory',
+    ];
+
+    public string $commandName = "db:table:truncate";
+
+    public string $commandDescription = "Allow to truncate db specific table.";
+    public function execute(): void
+    {
+        $tableName = $this->getParameter(2);
+
+        if (!$tableName) {
+            $this->output->error("A table should be specified.");
+
+            return;
+        }
+
+        if (!in_array($tableName, self::DB_TABLES)) {
+            $this->output->error("$tableName table does not exist.");
+
+            return;
+        }
+
+        $res = $this->conn->query("DELETE FROM $tableName;");
+
+        if (!$res) { return; }
+
+        $this->output->success("Migrations successfully executed.");
+    }
+}
