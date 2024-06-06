82048b38ebc2872965ffacf851e2b435996d8ed6
Guillian Aufrère <guillian77270@gmail.com>
2024-06-07 01:01:59 +0200

    fix: add missing config for flash plugins

--- source/backuper/usr/local/emhttp/plugins/backuper/config.php
+++ source/backuper/usr/local/emhttp/plugins/backuper/config.php
@@ -3,6 +3,7 @@
 return [
     "em_http_root" => getcwd() . "/plugins",
     "plugin_path"  => getcwd() . "/plugins/backuper",
+    "plugin_path_boot" => "/boot/config/plugins/backuper",
     "view_path"    => getcwd() . "/plugins/backuper/views",
 
     "dev_mode"     => true,
